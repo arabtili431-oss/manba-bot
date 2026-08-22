38fe4a32c8e72fde9693ed8cfd01872dc9b0bfe4
arabtili431-oss <arabtili431@gmail.com>
2026-08-22 18:02:38 +0600

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [123456789]
-DATABASE_URL = os.getenv("DATABASE_URL", "")
-
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
